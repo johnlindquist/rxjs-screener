3278373872a30377387c09e6efb8b9121bb967db
John Lindquist <johnlindquist@gmail.com>
2023-08-29 17:39:59 -0600

    fix(comment): exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ const URL = `https://jsonplaceholder.typicode.com/todos/1`;
 @Component({
   selector: 'app-root',
   template: `<div>
+    <!-- Don't change anything in the template -->
     <button (click)="click$.next()">Load Todo</button>
     <button (click)="cancel$.next()">Cancel Load</button>
 
     <div>
-      {{ todoTitle$ | async }}
+      {{ todo$ | async }}
     </div>
   </div>`,
 })
@@ -18,8 +19,15 @@ export class AppComponent {
   click$ = new Subject<void>();
   cancel$ = new Subject<void>();
 
-  // Exercise: Create the todoTitle$ observable (Everything else would be already setup)
-  todoTitle$ = concat(
+  // Exercise: Create the todo$ observable (Everything else would be already setup)
+  /**
+   * 1. Display a initial text: "Waiting for click..."
+   * 2. Clicking the "Load Todo" changes todo to "Loading..."
+   * 3. The stream then waits for 1 second before fetching the todo
+   * 4. If the user clicks "cancel" before the todo is loaded, prevent the load and display a "Cancelled!" message
+   */
+
+  todo$ = concat(
     of('Waiting for click...'),
 
     this.click$.pipe(
